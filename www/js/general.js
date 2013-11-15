b7d57930a818f0897dd2835c6232e914d0cd72ff
Marcus Fischer <xmarcus89@yahoo.de>
2013-11-15 10:39:03 +0100

    Änderungen am Photo Code wie im Example.

--- www/js/general.js
+++ www/js/general.js
@@ -1,11 +1,13 @@
 function capturePhoto(){
-    navigator.camera.getPicture(photoSuccess, photoError);
+    navigator.camera.getPicture(photoSuccess, photoError, { quality: 50, destinationType: Camera.DestinationType.FILE_URI});
 }
 
 function photoSuccess(data){
+    alert(data);
     document.getElementById('cameraPic').src = data;
 }
 
 function photoError(data){
     alert('Error capturing photo .. :(');
+    alert(data);
 }
